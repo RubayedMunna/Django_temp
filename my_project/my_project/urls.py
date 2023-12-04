c25b3b96859d00a37978da7df5a70fa61c1b4de3
Rubayed All Islam (Munna) <66661757+RubayedMunna@users.noreply.github.com>
2023-12-04 13:55:08 +0600

    Add files via upload

---
+++ my_project/my_project/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('day2app/',include('day2app.urls')),
+    path('day3app/',include('day3app.urls')),
+]
